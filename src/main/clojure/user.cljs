9320a16d5e08bbef0c9d1959238f21677cedecac
Icaro Motta <icaro.ldm@gmail.com>
2021-07-13 12:30:21 -0300

    Create dev-only function to quickly create streams

--- src/main/clojure/user.cljs
+++ src/main/clojure/user.cljs
@@ -2,7 +2,8 @@
   (:require [portal.web :as portal]
             [re-frame.core :as re-frame]
             [re-frame.db :as re-frame-db]
-            [acme.web.app :as app]))
+            [acme.web.app :as app]
+            [acme.web.event.stream :as stream]))
 
 (defn setup-portal
   []
@@ -20,6 +21,19 @@
   (setup-portal))
 
 (init!)
+
+(defn create-stream [{:keys [recipient token amount duration duration-unit]}]
+  (re-frame/dispatch-sync [::stream/form-update-recipient-address recipient])
+  (re-frame/dispatch-sync [::stream/form-update-token-address token])
+  (re-frame/dispatch-sync [::stream/form-update-amount (or amount "5000")])
+  (re-frame/dispatch-sync [::stream/form-update-time (or duration "10")])
+  (re-frame/dispatch-sync [::stream/form-update-unit (or duration-unit "minute")])
+  (re-frame/dispatch [::stream/form-create]))
+
+(comment
+  (create-stream {:recipient "0x0"
+                  ;; Rinkeby TestnetDAI
+                  :token "0xc3dbf84Abb494ce5199D5d4D815b10EC29529ff8"}))
 
 (comment
   (tap> @re-frame-db/app-db))
