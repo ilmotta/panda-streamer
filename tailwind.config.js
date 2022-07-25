8d0a7091622d59c8a6168adba62bd56eee3e3fc1
Icaro Motta <icaro.ldm@gmail.com>
2022-07-25 11:16:22 -0300

    Upgrade TailwindCSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,14 @@
 module.exports = {
-  // Tailwind uses PurgeCSS behind the scenes.
-  purge: {
-    preserveHtmlElements: true,
-    content: ['build/public/index.html', 'src/main/clojure/acme/web/**/*.cljs'],
-    safelist: {
-      standard: [/^co-/, /^hover:bg-purple/]
-    },
-    options: { keyframes: false, fontFace: true }
-  },
+  content: [
+    'build/public/index.html',
+    'src/main/clojure/acme/web/**/*.cljs'
+  ],
+  safelist: [{ pattern: /^co-/ }],
   // Avoid using JIT because it has rough edges, .e.g Tailwind CSS nesting
   // doesn't work at all.
   //
   // mode: 'jit',
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'media',
   theme: {
     extend: {}
   },
